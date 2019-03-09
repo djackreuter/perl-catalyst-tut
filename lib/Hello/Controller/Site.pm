b2cdb35d9955757cedd83dcd7a7b6cd3ae4944b2
Dylan Reuter <djreuter45@gmail.com>
2019-03-09 12:17:48 -0600

    initial commit

---
+++ lib/Hello/Controller/Site.pm
@@ -0,0 +1,47 @@
+package Hello::Controller::Site;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Hello::Controller::Site - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched Hello::Controller::Site in Site.');
+}
+
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Dylan,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
